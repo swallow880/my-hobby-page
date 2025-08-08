c876d5c56c8529beef2dee9f6a0fe243ff5b0f16
swallow880 <ogaoga7@gmail.com>
2025-08-08 09:48:11 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('bookButton').addEventListener('click', function() {
+    alert('旅行からはさまざまな刺激を受けられるので最高です！');
+});
+
+document.getElementById('cycleButton').addEventListener('click', function() {
+    alert('サイクリングは健康にも良いし、気分転換になりますね！');
+});
